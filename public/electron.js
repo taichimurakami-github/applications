65abc3e3156ca576172b523d82ea4f5b4c4c82b6
Taichi Murakami <64308722+taichimurakami-github@users.noreply.github.com>
2021-07-29 01:31:54 +0900

    ARCHITECT.mdを追加、技術的な検討完了、制作準備

--- public/electron.js
+++ public/electron.js
@@ -1,9 +1,9 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const { electron, app, BrowserWindow } = require('electron')
 const path = require('path')
 const isDev = require("electron-is-dev");
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -29,7 +29,7 @@ function createWindow () {
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
   createWindow()
-  
+
   app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
