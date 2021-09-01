e94edf4c25320d0f3748d4fb57f499b453b6891a
Taichi <64308722+taichimurakami-github@users.noreply.github.com>
2021-09-01 16:53:16 +0900

    微調整

--- public/electron/main.js
+++ public/electron/main.js
@@ -267,7 +267,7 @@ ipcMain.handle("handle_loadAppLocalConfig", (event, arg) => {
 
     case "write":
       const newAppLocalConfig = {...appLocalConfig};
-      console.log(arg.content.value);
+      // console.log(arg.content.value);
 
       //switch文中にswitchをネストするとかいう最高に頭が悪い構造をしているので、
       //なんかいい方法を見つけたら変更したい
