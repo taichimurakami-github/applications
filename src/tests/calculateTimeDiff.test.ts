95970de3f122b2a9824a9fe1a5f4065c815bc296
taichi.murakami <tchi0017@gmail.com>
2022-05-05 11:50:31 +0900

    test src/hooks/calculateTimeDiff.ts and passed

---
+++ src/tests/calculateTimeDiff.test.ts
@@ -0,0 +1,18 @@
+import { calculateTimeDiff } from "../utils/calculateTimeDiff";
+
+describe("calculateTimeDiff", (): void => {
+  test("enter: 8:00:10, exit: 12:10:10 の返り値は [2,10,0] でなければならない", (): void => {
+    const response: number[] = calculateTimeDiff("8:00:10", "12:10:10");
+    expect(response).toStrictEqual([4, 10, 0]);
+  });
+
+  test("enter: 8:50:10, exit: 10:10:10 の返り値は [1,20,0] でなければならない", (): void => {
+    const response: number[] = calculateTimeDiff("8:50:10", "10:10:50");
+    expect(response).toStrictEqual([1, 20, 40]);
+  });
+
+  test("enter: 9:50:30, exit: 10:10:10 の返り値は [0,19,40] でなければならない", (): void => {
+    const response: number[] = calculateTimeDiff("9:50:30", "10:10:10");
+    expect(response).toStrictEqual([0, 19, 40]);
+  });
+});
--- src/utils/calculateTimeDiff.ts
+++ src/utils/calculateTimeDiff.ts
@@ -2,29 +2,26 @@ export const calculateTimeDiff = (
   enterTime: string,
   exitTime: string
 ): number[] => {
+  //引数チェック
+  if (!enterTime.includes(":") || !exitTime.includes(":")) {
+    throw new Error("invalid arg error");
+  }
+
   //時刻、分、秒の数値をそれぞれ数値化して、配列に格納
   const enterTimeArr = enterTime.split(":").map((val) => Number(val));
   const exitTimeArr = exitTime.split(":").map((val) => Number(val));
   const result: number[] = [0, 0, 0];
 
-  // step1: 秒を計算
-  result[2] = exitTimeArr[2] - enterTimeArr[2];
-
-  // step2: 分を計算
-  result[1] = exitTimeArr[1] - enterTimeArr[1];
-
-  if (result[2] < 0) {
-    result[2] += 60;
-    exitTimeArr[1] -= 1;
+  for (let i = 2; i >= 0; i--) {
+    result[i] = exitTimeArr[i] - enterTimeArr[i];
+    console.log(i, result);
+    if (result[i] < 0) {
+      result[i] += 60;
+      exitTimeArr[i - 1] -= 1;
+    }
   }
 
-  // step3: 時刻を計算
-  result[0] = exitTimeArr[0] - enterTimeArr[0];
-
-  if (result[1] < 0) {
-    result[1] += 60;
-    exitTimeArr[0] -= 1;
-  }
+  console.log(result);
 
   return result;
 };
