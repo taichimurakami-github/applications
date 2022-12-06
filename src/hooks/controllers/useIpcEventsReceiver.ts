9bc199cb0119d62257ab3901e02377e4a4b30eff
taichi.murakami <tchi0017@gmail.com>
2022-12-06 19:01:00 +0900

    Remove unused functions and console.log

--- src/hooks/controllers/useIpcEventsReceiver.ts
+++ src/hooks/controllers/useIpcEventsReceiver.ts
@@ -2,19 +2,8 @@ import { useEffect } from "react";
 
 export const useIpcEventsReceiver = () => {
   useEffect(() => {
-    console.log("onListenUpdateProcess listen on renderer");
-    console.log(window.electron);
-    window.electron.ipcRendererOn(
-      "app-update-process",
-      (event: any, content: any) => {
-        console.log(content);
-      }
-    );
-    window.electron.onListenUpdateProcess((event: any, content: any) => {
-      console.log(content);
-      const a = document.createElement("div");
-      a.innerText = ":ewipofjgt0prijgo9rfjpojiu-@vjhner-jhnbo9jh-n";
-      document.appendChild(a);
+    window.electron.onListenUpdateProcess((content: any) => {
+      console.log("content:", content);
     });
   }, []);
 };
